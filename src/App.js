c83d862848d901e38e1098b7a37859306f688718
Mohammad Khaefi <mkhaefi2500@yahoo.com>
2023-09-26 14:01:58 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+// import { useEffect, useState } from "react";
+import { useState } from "react";
+import "./App.css";
+import SingleCard from "./components/SingleCard";
+// import image from "../public/img/ring-1.png";
+import cover from "./cover.png";
+const cardImages = [
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
+];
+
+const App = () => {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const shuffleCard = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffleCards);
+    setTurns(0);
+  };
+
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  return (
+    <div>
+      <h1>Memory Game</h1>
+      <button onClick={shuffleCard}>New Game</button>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
